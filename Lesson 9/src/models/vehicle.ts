2c5f364db8241d1ebfff7b19f084849a3b246dbb
Eugene Bezsrochnyi <e.bezsrochnyi@gmail.com>
2025-10-17 22:52:10 +0300

    add base structure and some implementation

---
+++ Lesson 9/src/models/vehicle.ts
@@ -0,0 +1,26 @@
+import { IEngine } from 'src/interfaces/i-engine';
+
+export abstract class Vehicle {
+    protected _brand: string;
+    protected _model: string;
+    protected _speed = 0;
+    protected _engine: IEngine;
+
+    public constructor(brand: string, model: string, engine: IEngine) {
+        this._brand = brand;
+        this._model = model;
+        this._engine = engine;
+    }
+
+    public getInfo(): string {
+        return `${this._brand} ${this._model} ${this._engine}`;
+    }
+
+    public abstract drive(): void;
+    public abstract reenergize(): void;
+
+    public accelerate(amount: number): void {
+        this._speed += amount;
+        console.log(`${this._brand} ${this._model} accelerates to ${this._speed} km/h`);
+    }
+}
