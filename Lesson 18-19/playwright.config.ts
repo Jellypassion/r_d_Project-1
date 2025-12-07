43b87bc79b00f76e32968fa64276593979deccd6
Eugene Bezsrochnyi <e.bezsrochnyi@gmail.com>
2025-12-07 20:38:17 +0200

    Implemented Allure for Playwright

--- Lesson 18-19/playwright.config.ts
+++ Lesson 18-19/playwright.config.ts
@@ -18,11 +18,11 @@ export default defineConfig({
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
+    retries: process.env.CI ? 0 : 0,
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
+    reporter: [['html'], ['allure-playwright']],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('')`. */
@@ -36,7 +36,8 @@ export default defineConfig({
     projects: [
         {
             name: 'chromium',
-            use: { ...devices['Desktop Chrome'] }
+            use: { ...devices['Desktop Chrome'],
+                headless: false }
         }
 
         // {
