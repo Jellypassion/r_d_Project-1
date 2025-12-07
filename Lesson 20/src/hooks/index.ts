3ebb4b45f8c45d36b0baa73b5ac783d9e3adb7c3
Eugene Bezsrochnyi <e.bezsrochnyi@gmail.com>
2025-12-07 13:40:15 +0200

    add attach-results hook

--- Lesson 20/src/hooks/index.ts
+++ Lesson 20/src/hooks/index.ts
@@ -3,10 +3,10 @@ import { browserHook } from './browser.hook.ts';
 import { globalContextHook } from './global-context.hook.ts';
 import { pageHook } from './page.hook.ts';
 import { browserContextHook } from './context.hook.ts';
-// import { attachResultsHook } from './attach-result.hook.ts';
+import { attachResultsHook } from './attach-result.hook.ts';
 
 globalContextHook();
 browserHook();
 browserContextHook();
 pageHook();
-// attachResultsHook();
+attachResultsHook();
